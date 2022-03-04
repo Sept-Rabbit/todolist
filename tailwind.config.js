5ed2d65b2b00de4360232b835bc5cd5471c808ce
搋兔 Tryto <35431381+huibizhang@users.noreply.github.com>
2022-03-04 09:56:45 +0800

    update: using plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value
+          }),
+        },
+        { values: theme('textShadow',{
+          'none': 'none',
+          'sm': 'black 0.05em 0.1em 0.2em'
+        }) }
+      )
+    })
+  ],
 };
